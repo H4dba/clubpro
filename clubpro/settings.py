bed6ec689c3b602915aed01cc04839beb966f0db
H4dba <gui.hadba24@gmail.com>
2025-10-27 21:47:39 -0300

    alterado para suportar supabase

--- clubpro/settings.py
+++ clubpro/settings.py
@@ -104,9 +104,17 @@ WSGI_APPLICATION = 'clubpro.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'old': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'clubpro_db'),
+        'USER': os.getenv('DB_USER', 'user'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'password'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
